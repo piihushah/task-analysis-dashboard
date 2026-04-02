b6ac5e436924c35bcf7b5eeb7f81377b0ac0182e
Piya-Shah <priya.shah@locafy.com>
2026-04-02 19:54:44 +0800

    Basic setup

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,12 @@
 export default function TaskCard({ task }) {
   const formatDate = (date) => new Date(date).toLocaleDateString();
   return (
-    <div className="flex flex-col gap-2 border-b-2 border-gray-200 py-4">
+    <div
+      className={`text-black bg-white p-4 rounded shadow-md mb-4 border-l-4 ${task.priority === "high" ? " border-red-500" : task.priority === "medium" ? " border-yellow-500" : " border-green-500"}`}
+    >
       <h4>{task.title}</h4>
-      <p>Status: {task.status}</p>
       <p>Category: {task.category}</p>
-      <p>Priority: {task.priority}</p>
       {task.createdAt && <p>Created At: {formatDate(task.createdAt)}</p>}
-      {task.completedAt && <p>Completed At: {formatDate(task.completedAt)}</p>}
     </div>
   );
 }
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,10 +3,22 @@ import TaskCard from "./TaskCard";
 
 export default function TaskList() {
   return (
-    <div>
-      {taskData.map((task) => (
-        <TaskCard key={task.id} task={task} />
-      ))}
+    <div className="py-12">
+      {/*pending | in_progress | completed: kanban board type column styling*/}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 border-2 border-gray-300 rounded-lg">
+        <div className="p-4 border-r-2 border-gray-300">
+          <h3 className="text-xl font-bold">Pending</h3>
+          {taskData.map((task) => task.status === "pending" && <TaskCard key={task.id} task={task} />)}
+        </div>
+        <div className="p-4 border-r-2 border-gray-300">
+          <h3 className="text-xl font-bold">In Progress</h3>
+          {taskData.map((task) => task.status === "in_progress" && <TaskCard key={task.id} task={task} />)}
+        </div>
+        <div className="p-4 border-r-2 border-gray-300">
+          <h3 className="text-xl font-bold">Completed</h3>
+          {taskData.map((task) => task.status === "completed" && <TaskCard key={task.id} task={task} />)}
+        </div>
+      </div>
     </div>
   );
 }
