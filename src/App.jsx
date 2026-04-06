15c3005faa698d3820cf0a45f4a01cbdf3542056
Piya-Shah <priya.shah@locafy.com>
2026-04-06 16:27:55 +0800

    App.jsx Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TaskAPI from "./components/TaskAPI";
 
 function App() {
   return (
-    <div className="container mx-auto md:max-w-7xl px-8 py-12 md:py-24">
+    <div className="container mx-auto md:max-w-7xl  px-4 md:px-8 py-12 md:py-24">
       <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold">Task Analytics Dashboard</h1>
       <h2 className="text-2xl font-bold mb-4">Dashboard & Task List</h2>
       <TaskAPI />
